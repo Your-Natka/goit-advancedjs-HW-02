4921b03d20a9c977ee229c5823f948f2b1177aca
Your-Natka <140067844+Your-Natka@users.noreply.github.com>
2026-05-02 21:25:09 +0200

    start

---
+++ src/js/1-tamer.js
@@ -0,0 +1,87 @@
+<a class="back-link" href="./index.html">
+  Go back
+</a>;
+import flatpickr from 'flatpickr';
+import 'flatpickr/dist/flatpickr.min.css';
+import iziToast from 'izitoast';
+import 'izitoast/dist/css/iziToast.min.css';
+
+const input = document.querySelector('#datetime-picker');
+const startBtn = document.querySelector('[data-start]');
+
+const daysEl = document.querySelector('[data-days]');
+const hoursEl = document.querySelector('[data-hours]');
+const minutesEl = document.querySelector('[data-minutes]');
+const secondsEl = document.querySelector('[data-seconds]');
+
+let userSelectedDate = null;
+let timerId = null;
+
+startBtn.disabled = true;
+
+function addLeadingZero(value) {
+  return String(value).padStart(2, '0');
+}
+
+function convertMs(ms) {
+  const second = 1000;
+  const minute = second * 60;
+  const hour = minute * 60;
+  const day = hour * 24;
+
+  const days = Math.floor(ms / day);
+  const hours = Math.floor((ms % day) / hour);
+  const minutes = Math.floor(((ms % day) % hour) / minute);
+  const seconds = Math.floor((((ms % day) % hour) % minute) / second);
+
+  return { days, hours, minutes, seconds };
+}
+
+function updateUI({ days, hours, minutes, seconds }) {
+  daysEl.textContent = addLeadingZero(days);
+  hoursEl.textContent = addLeadingZero(hours);
+  minutesEl.textContent = addLeadingZero(minutes);
+  secondsEl.textContent = addLeadingZero(seconds);
+}
+
+const options = {
+  enableTime: true,
+  time_24hr: true,
+  defaultDate: new Date(),
+  minuteIncrement: 1,
+  onClose(selectedDates) {
+    const date = selectedDates[0];
+
+    if (date <= new Date()) {
+      iziToast.error({
+        message: 'Please choose a date in the future',
+      });
+      startBtn.disabled = true;
+      return;
+    }
+
+    userSelectedDate = date;
+    startBtn.disabled = false;
+  },
+};
+
+flatpickr(input, options);
+
+startBtn.addEventListener('click', () => {
+  startBtn.disabled = true;
+  input.disabled = true;
+
+  timerId = setInterval(() => {
+    const now = new Date();
+    const diff = userSelectedDate - now;
+
+    if (diff <= 0) {
+      clearInterval(timerId);
+      updateUI({ days: 0, hours: 0, minutes: 0, seconds: 0 });
+      input.disabled = false;
+      return;
+    }
+
+    updateUI(convertMs(diff));
+  }, 1000);
+});
